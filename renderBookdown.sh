ceacab77324a1f1f4fd6cdebf5641a726ced8bf5
Simon Goring <simon.j.goring@gmail.com>
2022-06-27 15:12:41 -0700

    Adding new edits & tables.

--- renderBookdown.sh
+++ renderBookdown.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env Rscript
-Rscript -e "bookdown::render_book()" && rm -r ~/Documents/Neotoma/neotomadb.github.io/manual/* && mv ./_book/* ~/Documents/Neotoma/neotomadb.github.io/manual
+Rscript -e "bookdown::render_book()" && rm -r ~/Documents/Neotoma/neotomadb.github.io/manual/* && cp -r ./_book/* ~/Documents/Neotoma/neotomadb.github.io/manual
